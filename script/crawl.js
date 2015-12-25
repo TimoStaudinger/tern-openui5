0b8ed9f76a22cc2a2f12620b026078e9a46cffac
Timo Staudinger <staudinger.timo@gmail.com>
2015-12-24 19:10:27 -0500

    Introduce Standard code style

--- script/crawl.js
+++ script/crawl.js
@@ -1,12 +1,12 @@
-"use strict"
+'use strict'
 
-let request = require('request')
-let parseXmlString = require('xml2js').parseString
 let cheerio = require('cheerio')
+let dirname = require('path').dirname
+let format = require('standard-format').transform
 let fs = require('fs')
 let mkdirp = require('mkdirp')
-let dirname = require('path').dirname
-let indent = require('indent-string')
+let parseXmlString = require('xml2js').parseString
+let request = require('request')
 
 const INDEX_URL = 'https://openui5.hana.ondemand.com/docs/api/index.xml'
 const DETAIL_URL_PREFIX = 'https://openui5.hana.ondemand.com/docs/api/'
@@ -18,55 +18,56 @@ const OPENUI5_OUT = '../openui5.js'
 const MAX_LEVEL = 20
 
 let tree = {
-  "!name": "openui5"
+  '!name': 'openui5'
 }
-let promisesDone = 0;
-let lastShownPercentage = -1
+
 let readDetailsQueue = []
 let retryQueue = []
 
-let mapTypeToTern = function(type) {
+let mapTypeToTern = function (type) {
   let types = type.split('|')
-  if(types.length === 0) return ''
+  if (types.length === 0) return ''
   type = types[0]
 
   const ARRAY_PATTERN = [
     /(.*)\[\]/, // integer[]
     /array\((.*)\)/ // array(integer)
   ]
-  let isArray = ARRAY_PATTERN.some(function(regex) {
+  let isArray = ARRAY_PATTERN.some(function (regex) {
     let matches = regex.exec(type)
-    if(matches && matches[1]) {
+    if (matches && matches[1]) {
       type = matches[1]
       return true
     }
   })
 
-  if(type === 'int') type = 'number'
-  if(type === 'boolean') type = 'bool'
-  if(type === '*') type = '?'
+  if (type === 'int') type = 'number'
+  if (type === 'boolean') type = 'bool'
+  if (type === '*') type = '?'
 
   if (isArray) type = '[' + type + ']'
 
   return type
 }
 
-let buildTree = function(objects, currentRoot, level) {
+let buildTree = function (objects, currentRoot, level) {
   level = level || 0
-  objects.forEach(function(object) {
+  objects.forEach(function (object) {
     let branch = currentRoot[object.name[0]] = {}
-    readDetailsQueue.push({ref: object.ref[0], branch: branch})
-    if(level < MAX_LEVEL && !!object.children) buildTree(object.children[0].namespace, branch, level + 1)
+    readDetailsQueue.push({
+      ref: object.ref[0],
+      branch: branch
+    })
+    if (level < MAX_LEVEL && !!object.children) buildTree(object.children[0].namespace, branch, level + 1)
   })
 }
 
-let readAttributesForNode = function(ref, branch) {
+let readAttributesForNode = function (ref, branch) {
   let detailUrl = DETAIL_URL_PREFIX + ref
 
-  let promise = new Promise(function(resolve, reject){
-
+  let promise = new Promise(function (resolve, reject) {
     request(detailUrl, function (error, response, data) {
-      if (!error && response.statusCode == 200) {
+      if (!error && response.statusCode === 200) {
         let $ = cheerio.load(data)
         let doc = $('div.full-description > p:first-of-type').text().trim()
         let url = DOC_URL_PREFEX + ref
@@ -74,7 +75,7 @@ let readAttributesForNode = function(ref, branch) {
         ext = ext ? ext.trim() : null
 
         let constructor = null
-        $('div.sectionTitle:contains(" Constructor Detail ") + div.sectionItems div.sectionItem').each(function(i, constructorElement) {
+        $('div.sectionTitle:contains(" Constructor Detail ") + div.sectionItems div.sectionItem').each(function (i, constructorElement) {
           constructor = {
             arguments: []
           }
@@ -93,13 +94,11 @@ let readAttributesForNode = function(ref, branch) {
               isOptional: argumentIsOptional
             })
           })
-
-
         })
 
         let methods = []
         let methodsElement = $('div.sectionTitle:contains(" Method Detail ") + div.sectionItems')
-        cheerio('div.sectionItem', methodsElement).each(function(i, method) {
+        cheerio('div.sectionItem', methodsElement).each(function (i, method) {
           let methodNameFullyQualified = cheerio('div.itemTitle', method).clone().children().remove().end().text().trim().split('.')
           let methodIsStatic = methodNameFullyQualified.length > 1
           let methodName = methodNameFullyQualified[methodNameFullyQualified.length - 1]
@@ -109,7 +108,7 @@ let readAttributesForNode = function(ref, branch) {
 
           let methodArguments = []
           let argumentsElement = cheerio('div.heading:contains("Parameters:") + table.methodItem > tbody > tr', method)
-          argumentsElement.each(function(i, argument) {
+          argumentsElement.each(function (i, argument) {
             let argumentName = cheerio('td.methodItemName > b', argument).text().trim()
             let argumentIsOptional = cheerio('td.methodItemName > i.help', argument).attr('title') === 'Optional parameter'
             let argumentType = cheerio('td.methodItemType', argument).text().trim()
@@ -136,35 +135,47 @@ let readAttributesForNode = function(ref, branch) {
           })
         })
 
-        resolve({doc: doc, url: url, constructor: constructor, methods: methods, extends: ext})
+        resolve({
+          doc: doc,
+          url: url,
+          constructor: constructor,
+          methods: methods,
+          extends: ext
+        })
       } else {
-        reject({reason: error.code, ref: ref, branch: branch})
+        reject({
+          reason: error.code,
+          ref: ref,
+          branch: branch
+        })
       }
     })
-  }).then(function(data){
+  }).then(function (data) {
     branch['!doc'] = data.doc
     branch['!url'] = data.url
-    if(data.extends) {
+    if (data.extends) {
       branch.prototype = {
-        "!proto": data.extends + '.prototype'
+        '!proto': data.extends + '.prototype'
       }
     }
     addConstructorToBranch(data.constructor, branch)
     addMethodsToBranch(data.methods, branch)
-    if(branch.prototype && Object.keys(branch.prototype).length === 0) delete branch.prototype
-  }).catch(function(data){
+    if (branch.prototype && Object.keys(branch.prototype).length === 0) delete branch.prototype
+  }).catch(function (data) {
     console.log('Warning: ' + data.reason + ' for ' + data.ref + ' -- will retry')
-    retryQueue.push({ref: data.ref, branch: data.branch})
+    retryQueue.push({
+      ref: data.ref,
+      branch: data.branch
+    })
   })
   return promise
 }
 
-let addConstructorToBranch = function(constructor, branch) {
-  if(constructor) {
-
+let addConstructorToBranch = function (constructor, branch) {
+  if (constructor) {
     let type = 'fn('
-    type += constructor.arguments.map(function(arg){
-      if(arg.name.includes('.')) return null
+    type += constructor.arguments.map(function (arg) {
+      if (arg.name.includes('.')) return null
       let argString = arg.name
       if (arg.isOptional) argString += '?'
       argString += ': ' + mapTypeToTern(arg.type)
@@ -176,21 +187,21 @@ let addConstructorToBranch = function(constructor, branch) {
   }
 }
 
-let addMethodsToBranch = function(methods, branch) {
+let addMethodsToBranch = function (methods, branch) {
   branch.prototype = branch.prototype || {}
 
-  methods.forEach(function(method) {
+  methods.forEach(function (method) {
     let type = 'fn('
-    type += method.arguments.map(function(arg){
-      if(arg.name.includes('.')) return null
+    type += method.arguments.map(function (arg) {
+      if (arg.name.includes('.')) return null
       let argString = arg.name
       if (arg.isOptional) argString += '?'
       argString += ': ' + mapTypeToTern(arg.type)
       return argString
     }).join(', ')
     type += ')'
-    if(!!method.returns) type += ' -> ' + mapTypeToTern(method.returns)
-    if(method.isStatic) {
+    if (method.returns) type += ' -> ' + mapTypeToTern(method.returns)
+    if (method.isStatic) {
       branch[method.name] = {
         '!doc': method.doc,
         '!url': method.url,
@@ -206,18 +217,18 @@ let addMethodsToBranch = function(methods, branch) {
   })
 }
 
-let readAttributes = function() {
-  return new Promise(function(resolve, reject){
+let readAttributes = function () {
+  return new Promise(function (resolve, reject) {
     console.log('Reading details of ' + readDetailsQueue.length + ' entries...')
     let promises = []
-    readDetailsQueue.forEach(function(entry) {
+    readDetailsQueue.forEach(function (entry) {
       promises.push(readAttributesForNode(entry.ref, entry.branch))
     })
-    Promise.all(promises).then(function() {
+    Promise.all(promises).then(function () {
       readDetailsQueue = retryQueue
       retryQueue = []
 
-      if(readDetailsQueue.length > 0) {
+      if (readDetailsQueue.length > 0) {
         readAttributes().then(resolve)
       } else {
         resolve()
@@ -228,51 +239,67 @@ let readAttributes = function() {
 
 console.log('Loading index file at ' + INDEX_URL)
 
-new Promise(function(resolve, reject) {
+new Promise(function (resolve, reject) {
   request(INDEX_URL, function (error, response, data) {
-    if (!error && response.statusCode == 200) {
+    if (!error && response.statusCode === 200) {
       resolve(data)
     } else {
       reject('Error while loading index file: ' + error.code)
     }
   })
-}).then(function(data) {
-  return new Promise(function(resolve, reject) {
-    parseXmlString(data, function(err, result) {
-      resolve(result.index)
+}).then(function (data) {
+  return new Promise(function (resolve, reject) {
+    parseXmlString(data, function (err, result) {
+      if (err) {
+        reject('Failed to parse index: ' + err)
+      } else {
+        resolve(result.index)
+      }
     })
   })
-}).then(function(data) {
+}).then(function (data) {
   console.log('Parsing index...')
   buildTree(data.namespace, tree)
-}).then(function(){
+}).then(function () {
   return readAttributes()
 }).then(function () {
   console.log('Writing output...')
 
-  fs.readFile(OPENUI5_TEMPLATE, 'utf8', function(err, data){
+  fs.readFile(OPENUI5_TEMPLATE, 'utf8', function (err, data) {
     if (err) console.log('Error while writing output: ' + err)
     else {
       let stringifiedTree = JSON.stringify(tree, null, 2)
-      let openui5Content = data.replace('"{{defs}}"', indent(stringifiedTree, ' ', 2))
+      let openui5Content = format(data.replace("'{{defs}}'", stringifiedTree), {indent_size: 2})
 
       mkdirp(dirname(OPENUI5_OUT), function (err) {
-        if (err) console.log('Error while writing ' + OPENUI5_OUT + ': ' + err)
-        else fs.writeFile(OPENUI5_OUT, openui5Content, function(err) {
-          if (err) console.log('Error while writing ' + OPENUI5_OUT + ': ' + err)
-          else console.log("Tern plugin written to " + OPENUI5_OUT)
-        })
+        if (err) {
+          console.log('Error while writing ' + OPENUI5_OUT + ': ' + err)
+        } else {
+          fs.writeFile(OPENUI5_OUT, openui5Content, function (err) {
+            if (err) {
+              console.log('Error while writing ' + OPENUI5_OUT + ': ' + err)
+            } else {
+              console.log('Tern plugin written to ' + OPENUI5_OUT)
+            }
+          })
+        }
       })
 
       mkdirp(dirname(TREE_OUT), function (err) {
-        if (err) console.log('Error while writing ' + TREE_OUT + ': ' + err)
-        else fs.writeFile(TREE_OUT, stringifiedTree, function(err) {
-          if (err) console.log('Error while writing ' + TREE_OUT + ': ' + err)
-          else console.log("Tree written to " + TREE_OUT)
-        })
+        if (err) {
+          console.log('Error while writing ' + TREE_OUT + ': ' + err)
+        } else {
+          fs.writeFile(TREE_OUT, stringifiedTree, function (err) {
+            if (err) {
+              console.log('Error while writing ' + TREE_OUT + ': ' + err)
+            } else {
+              console.log('Tree written to ' + TREE_OUT)
+            }
+          })
+        }
       })
     }
   })
-}).catch(function(reason) {
+}).catch(function (reason) {
   console.log(reason)
 })
--- script/openui5-template.js
+++ script/openui5-template.js
@@ -1,15 +1,21 @@
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    return mod(require("tern/lib/infer"), require("tern/lib/tern"), require);
-  if (typeof define == "function" && define.amd) // AMD
-    return define(["tern/lib/infer", "tern/lib/tern"], mod);
-  mod(tern, tern);
-})(function(infer, tern, require) {
-  "use strict";
+/* global define, tern */
 
-  tern.registerPlugin("openui5", function(server, options) {
-    return {defs: defs};
-  });
+(function (mod) {
+  if (typeof exports === 'object' && typeof module === 'object') { // CommonJS
+    return mod(require('tern/lib/infer'), require('tern/lib/tern'), require)
+  }
+  if (typeof define === 'function' && define.amd) { // AMD
+    return define(['tern/lib/infer', 'tern/lib/tern'], mod)
+  }
+  mod(tern, tern)
+})(function (infer, tern, require) {
+  'use strict'
 
-  var defs = "{{defs}}";
-});
+  tern.registerPlugin('openui5', function (server, options) {
+    return {
+      defs: defs
+    }
+  })
+
+  var defs = '{{defs}}'
+})
